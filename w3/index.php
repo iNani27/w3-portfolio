f17a9bddce650fe62a6c1639ec0c4730201de1d7
Isabelle Nani <webdeveloperinani@gmail.com>
2016-07-14 23:35:43 +0200

    Update index.php

--- w3/index.php
+++ w3/index.php
@@ -22,14 +22,14 @@ include_once 'view/inc/meta.php';
             <span class="w3-opennav w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
             <h1 class="w3-animate-opacity w3-font-3 w3-text-teal w3-lobster font-effect-brick-sign"><b>Mon portfolio</b></h1>
 
-            <div class="w3-section w3-bottombar w3-padding-16">
+           <!-- <div class="w3-section w3-bottombar w3-padding-16">
                 <span>Catégories :  </span> 
                 <button class="w3-btn w3-disabled"> All </button>
                 <button class="w3-btn w3-disabled w3-white"><i class="fa fa-code"></i>  Development </button>
                 <button class="w3-btn w3-disabled w3-white w3-hide-small"><i class="fa fa-diamond"></i>  Design</button>
                 <button class="w3-btn w3-disabled w3-white w3-hide-small"><i class="fa fa-map-pin"></i>  Art</button>
             </div>
-
+-->
 
         </header>
 
